efd45c0542b180b45bb9535af828f0d85e11c3fc
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-12-14 03:10:33 +0000

    1st commit

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+# Create an empty list to store the numbers
 numbers = []
 
+# Ask the user to input 5 numbers
 for i in range(5):
-    numbers.append(int(input()))
-total = sum(numbers)
-toatl = total - min(numbers) - max(numbers)
-print(total)
+  # Take user input and append it to the list
+  numbers.append(int(input("Enter a number: ")))
+
+# Calculate the min and max values in the list
+min_value = min(numbers)
+max_value = max(numbers)
+
+# Calculate the sum of all values in the list except the min and max
+total = sum(numbers) - min_value - max_value
+
+# Print the result
+print("The total of all numbers except the min and max is: ", total)
